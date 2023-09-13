8ce99a18b019343648c2b20f01509eba7a75bb73
sojin-p <luv2hong@gmail.com>
2023-09-13 11:43:54 +0900

    230913 제네릭으로 변경

--- UnsplashProject/Calculate/CalculateViewController.swift
+++ UnsplashProject/Calculate/CalculateViewController.swift
@@ -18,47 +18,49 @@ class CalculateViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let person = Person("새싹이") //이건 최초 값 들어가는 거라 프린트가 찍히지 않음(초기화 한거라)
-        
-        //바뀔 때마다 didSet 호출
-        person.name = "커스타드"
-        person.name = "칙촉"
-        
-        //마지막 이름 기준으로 인트로듀스가 붙여짐
-        //person.introduce(Int.random(in: 1...10))
-        
-        //(5) 이게 계속 실행이 여러번 되고 있는 것이 아니다!
-        //intro함수 안에(sample(name)) 변수name 업데이트하는 걸(listener = sample) 넣었기 때문에, 이름이 바뀌게 되고, name이 바뀌니까 name didSet에 listener?(name) 호출되는 것!
-        person.introduce { name in
-            self.resultLabel.text = name
-            self.view.backgroundColor = [UIColor.blue, UIColor.orange, UIColor.cyan].randomElement()!
-        }
-        
-        //5-1. 인트로듀스가 들어오면 백그라운드를 바꾸고싶다...(5-2로!)
-//        person.introduce(Int.random(in: 1...10)) {
-//            self.view.backgroundColor = [UIColor.blue, UIColor.orange, UIColor.cyan].randomElement()!
-//        }
-        
-        //10. 배경색이 바뀌는건지 체크가 안되니까 딜레이 주기
-        //언제쓰나? - 검색기능, 당겨서 새로고침, 로딩바 띄우고 싶을 때(통신이 빠르면 안 보임) 최소 0.5초는 보여주자
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            person.name = "바나나"
-            print("----1초뒤에 찍히나?---")
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            person.name = "키위"
-            print("----3초뒤에 찍히나?---")
-        }
-        
-        //4. 럭키넘버 변수 생성 후, 인트로듀스 생성 이후라서 숫자가 들어가기 시작!
-        //person.name = "바나나"
-        
-        //VC는 내용을 모른다. 그저 가져와서 보여주는 것뿐
-        firstTextField.text = viewModel.firstNumber.value
-        secondTextField.text = viewModel.secondNumber.value
-        
     }
     
 
 }
+
+/*
+ let person = Person("새싹이") //이건 최초 값 들어가는 거라 프린트가 찍히지 않음(초기화 한거라)
+ 
+ //바뀔 때마다 didSet 호출
+ person.name = "커스타드"
+ person.name = "칙촉"
+ 
+ //마지막 이름 기준으로 인트로듀스가 붙여짐
+ //person.introduce(Int.random(in: 1...10))
+ 
+ //(5) 이게 계속 실행이 여러번 되고 있는 것이 아니다!
+ //intro함수 안에(sample(name)) 변수name 업데이트하는 걸(listener = sample) 넣었기 때문에, 이름이 바뀌게 되고, name이 바뀌니까 name didSet에 listener?(name) 호출되는 것!
+ person.introduce { name in
+     self.resultLabel.text = name
+     self.view.backgroundColor = [UIColor.blue, UIColor.orange, UIColor.cyan].randomElement()!
+ }
+ 
+ //5-1. 인트로듀스가 들어오면 백그라운드를 바꾸고싶다...(5-2로!)
+//        person.introduce(Int.random(in: 1...10)) {
+//            self.view.backgroundColor = [UIColor.blue, UIColor.orange, UIColor.cyan].randomElement()!
+//        }
+ 
+ //10. 배경색이 바뀌는건지 체크가 안되니까 딜레이 주기
+ //언제쓰나? - 검색기능, 당겨서 새로고침, 로딩바 띄우고 싶을 때(통신이 빠르면 안 보임) 최소 0.5초는 보여주자
+ DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+     person.name = "바나나"
+     print("----1초뒤에 찍히나?---")
+ }
+ 
+ DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+     person.name = "키위"
+     print("----3초뒤에 찍히나?---")
+ }
+ 
+ //4. 럭키넘버 변수 생성 후, 인트로듀스 생성 이후라서 숫자가 들어가기 시작!
+ //person.name = "바나나"
+ 
+ //VC는 내용을 모른다. 그저 가져와서 보여주는 것뿐
+ firstTextField.text = viewModel.firstNumber.value
+ secondTextField.text = viewModel.secondNumber.value
+ */
--- UnsplashProject/Calculate/SojinObservable.swift
+++ UnsplashProject/Calculate/SojinObservable.swift
@@ -7,24 +7,24 @@
 
 import Foundation
 
-class Person {
+class CustomObservable<T> { //1. 제네릭으로 T
     
-    var listener: ((String) -> Void)? //(2) 여기도 name의 타입 넣기
+    private var listener: ((T) -> Void)?
     
-    var name: String {
+    var value: T { //2. name에서 value로 바꾸기
         didSet {
-            listener?(name) //(4) name 추가
-            print("사용자의 이름이 \(name)(으)로 변경되었습니다.")
+            listener?(value) //3. 데이터를 바인딩 하는 역할. 데이터가 바뀔때마다 함수가 실행 되니까!
+            print("사용자의 이름이 \(value)(으)로 변경되었습니다.")
         }
     }
     
-    init(_ name: String) {
-        self.name = name
+    init(_ value: T) {
+        self.value = value
     }
     
-    func introduce(_ sample: @escaping (String) -> Void) { //(1) 사용자가 알 수 있게 name의 타입인 String 전달하자
-        print("저는 \(name)입니다.")
-        sample(name) //(3) 이름을 보여주고 싶어서 name추가 +만약 이게 없다면 바나나 부터 실행이 되는 것! 왜? VC의 인트로듀스 안의 클로저가 실행되는게 아니므로.
+    func introduce(_ sample: @escaping (T) -> Void) {
+        print("저는 \(value)입니다.")
+        sample(value)
         listener = sample
     }
     
